005447ba7cdb348591b2dd9517a0d10896058b70
l14nost <amirbanor@gmail.com>
2023-08-29 18:29:13 +0300

    Initial commit

---
+++ src/main/java/lab/space/my_house_24_site/MyHouse24SiteApplication.java
@@ -0,0 +1,13 @@
+package lab.space.my_house_24_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHouse24SiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHouse24SiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/space/my_house_24_site/MyHouse24SiteApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.space.my_house_24_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHouse24SiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
